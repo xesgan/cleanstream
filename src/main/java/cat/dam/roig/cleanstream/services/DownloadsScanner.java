5be88c33efc319fe24600064d4bf2f051127112d
xesgan <h4ku.era@gmail.com>
2025-10-29 10:51:38 +0100

    feat(service): crear clase DownloadsScanner

--- src/main/java/cat/dam/roig/cleanstream/services/DownloadsScanner.java
+++ src/main/java/cat/dam/roig/cleanstream/services/DownloadsScanner.java
@@ -6,4 +6,6 @@ package cat.dam.roig.cleanstream.services;
  */
 public class DownloadsScanner {
     
+    // Falta logica carpeta de descarga y persistencia de las rutas
+    // No se como voy a hacer esto de la persistencia
 }
