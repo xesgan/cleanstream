816b69855bd8e4e71c399cd4dbc27a5571f85a59
xesgan <h4ku.era@gmail.com>
2025-10-29 10:12:48 +0100

    feat(modelo): crear clase ResourceDownloaded

---
+++ src/main/java/cat/dam/roig/cleanstream/models/ResourceDownloaded.java
@@ -0,0 +1,80 @@
+package cat.dam.roig.cleanstream.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author metku
+ */
+public class ResourceDownloaded {
+    
+    private String name;
+    private String route;
+    private long size;
+    private String mimeType;
+    private LocalDateTime downloadDate;
+    private String extension;
+
+    public ResourceDownloaded(String name, String route, long size, String mimeType, LocalDateTime downloadDate, String extension) {
+        this.name = name;
+        this.route = route;
+        this.size = size;
+        this.mimeType = mimeType;
+        this.downloadDate = downloadDate;
+        this.extension = extension;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+    
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public LocalDateTime getDownloadDate() {
+        return downloadDate;
+    }
+
+    public void setDownloadDate(LocalDateTime downloadDate) {
+        this.downloadDate = downloadDate;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    @Override
+    public String toString() {
+        return "RecursoDescargado{" + "name=" + name + ", route=" + route + ", size=" + size + ", mimeType=" + mimeType + ", downloadDate=" + downloadDate + ", extension=" + extension + '}';
+    }
+    
+}
