4038d0ebfca0215f39677492cf28267603a09c4d
xesgan <h4ku.era@gmail.com>
2026-02-26 15:07:36 +0100

    docs: add detailed Javadoc to MediaUpdateListener

--- src/main/java/cat/dam/roig/cleanstream/services/polling/MediaUpdateListener.java
+++ src/main/java/cat/dam/roig/cleanstream/services/polling/MediaUpdateListener.java
@@ -1,10 +1,40 @@
 package cat.dam.roig.cleanstream.services.polling;
 
 /**
- * Listener for media update events.
+ * Listener interface used to receive media update notifications from
+ * {@link MediaPolling} implementations.
+ *
+ * <p>
+ * This interface is part of the Observer pattern used in CleanStream. When the
+ * polling component detects new media in the cloud, it notifies all registered
+ * listeners through this interface.
+ * </p>
+ *
+ * <p>
+ * Typical usage:
+ * <pre>
+ * mediaPolling.addMediaListener(event -> {
+ *     System.out.println("New media detected: " + event.getNewMediaCount());
+ *     refreshUi();
+ * });
+ * </pre>
+ *
+ * <p>
+ * Threading note: Implementations should be aware that this callback may be
+ * invoked from a background thread depending on the polling implementation. If
+ * UI updates are required, they must be executed on the Swing Event Dispatch
+ * Thread (EDT) using {@code SwingUtilities.invokeLater()}.
+ * </p>
+ *
+ * @author metku
  */
 @FunctionalInterface
 public interface MediaUpdateListener {
 
+    /**
+     * Called when new media is detected in the cloud.
+     *
+     * @param event event containing update information
+     */
     void onMediaUpdate(MediaUpdateEvent event);
 }
